5dcdba8e48367bebe501e001bff92df6a098e034
Mohamed2Thabet <109123303+Mohamed2Thabet@users.noreply.github.com>
2025-03-17 10:25:38 +0200

    "Fix GitHub Pages deployment"

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/Mahd-E-Learning-Platform/', // تأكد من تغيير هذا بناءً على اسم الـ repository
+  plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/Mahd-E-Learning-Platform/" : "/",
 });
