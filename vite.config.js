80866fc785770fe9f850e01765f27b817aecc871
Mohamed2Thabet <109123303+Mohamed2Thabet@users.noreply.github.com>
2025-03-17 10:16:52 +0200

    git commit -m "Fix GitHub Pages deployment"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/Mahd-E-Learning-Platform/', // تأكد من تغيير هذا بناءً على اسم الـ repository
+});
